7de5c5e344ce0bbe58c1fd18c8f07ff0c80b1e50
= <ja.gustafik@outlook.sk>
2024-01-06 07:29:14 +0000

    First remap

---
+++ lua/kubgus/remap.lua
@@ -0,0 +1,22 @@
+local function map(mode, lhs, rhs, opts)
+	local options = { noremap=true, silent=true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local function cmd(inp)
+	local res =  ":" .. inp .. "<CR>"
+	return res
+end
+
+-- Change leader to space
+vim.g.mapleader = " "
+
+------------------------------------------------------
+-- NeoVim Shortcuts
+------------------------------------------------------
+
+-- Netrw easy access
+map("n", "<leader>pv", cmd("Ex"))
